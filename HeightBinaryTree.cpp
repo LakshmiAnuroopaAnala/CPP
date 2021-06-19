31541b9766d0c8d2ae384cee64a5d981dc634e0c
Lakshmi Anuroopa Anala <62070782+LakshmiAnuroopaAnala@users.noreply.github.com>
2021-06-19 09:00:55 +0530

    Add files via upload

---
+++ HeightBinaryTree.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *left;
+	Node *right;
+	Node(int val)
+	{
+		data=val;
+		left=right=NULL;
+	}
+};
+int height(Node *root)
+{
+	if(root==NULL)
+	{
+		return 0;
+	}
+	return max(height(root->right),height(root->left))+1;
+}
+int main()
+{
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	cout<<height(root);
+	return 0;
+}
